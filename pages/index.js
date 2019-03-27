2dc99742a4a27124f59bbed0501a24d8335f3797
Orion Xiao <opx200@stern.nyu.edu>
2019-03-27 13:53:31 -0400

    homepage layout

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import Header from "../components/Header";
+import {Main} from '../styles';
 
 function Index() {
     return (
-        <main>
+        <Main>
             <Header />
             <section>
                 <Link href="/about">
                     <a>Go to About Me</a>
                 </Link>
             </section>
-        </main>
+        </Main>
     );
 }
 
