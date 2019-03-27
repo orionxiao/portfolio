c193b8560fa632a4492e968d5f3c431dbfe2f656
Orion Xiao <opx200@stern.nyu.edu>
2019-03-27 13:29:26 -0400

    testing layout

--- components/header.js
+++ components/header.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 function Header() {
   return (
     <header>
       <h1>Orion Xiao</h1>
+      <Link href="/">Home</Link>
+      <Link href="/about">About Me</Link>
     </header>
   );
 }
